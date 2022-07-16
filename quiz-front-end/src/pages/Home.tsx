b421483b4ed216087367cf15802bbe8d5198a12c
Landon Schlangen <landon.schlangen@airtonomy.ai>
2022-07-16 10:57:52 -0500

    Added routing, home page

---
+++ quiz-front-end/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Home() {
+    return (
+        <div className="">
+            <div className="text-center">
+                <h1 className='my-8'>Welcome to Quiz Maker!</h1>
+                <button className="btn btn-lg btn-primary">Get Started!</button>
+            </div>
+        </div>
+    )
+}
